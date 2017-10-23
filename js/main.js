f0462d0906ccba03399a3a5a2778ababef42bd3d
e1bandito <30836258+e1bandito@users.noreply.github.com>
2017-10-23 19:39:33 +0300

    Позиционирование модальных окон и правки по линтеру в js.

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ header.classList.add("page-header__opacity");
 mainNav.classList.add("main-nav__hide");
 
 button.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	button.classList.toggle("main-nav__burger");
-	header.classList.toggle("page-header__opacity");
+  evt.preventDefault();
+  button.classList.toggle("main-nav__burger");
+  header.classList.toggle("page-header__opacity");
   mainNav.classList.toggle("main-nav__hide");
 });
 
@@ -20,8 +20,8 @@ if (imagePosition) {
   imagePosition.classList.add("intro__image-position");
   titlePosition.classList.add("intro__title--position");
   button.addEventListener("click", function (evt) {
-  	evt.preventDefault();
-  	imagePosition.classList.toggle("intro__image-position");
+    evt.preventDefault();
+    imagePosition.classList.toggle("intro__image-position");
     titlePosition.classList.toggle("intro__title--position");
   });
 }
@@ -29,7 +29,7 @@ if (imagePosition) {
 if (marketImage) {
   marketImage.classList.add("market__image-position");
   button.addEventListener("click", function (evt) {
-  	evt.preventDefault();
-  	marketImage.classList.toggle("market__image-position");
+    evt.preventDefault();
+    marketImage.classList.toggle("market__image-position");
   });
 }
